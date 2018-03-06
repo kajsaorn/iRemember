e045a5016e67347d7f8d269303b4336af95e571b
KarlEbbe <karlebbej@gmail.com>
2018-03-06 14:19:44 +0100

    new try

---
+++ iRememberMaster/app/src/main/java/com/iremember/master/iremembermaster/Constants/Protocol.java
@@ -0,0 +1,10 @@
+package com.iremember.master.iremembermaster.Constants;
+
+/**
+ * Created by KEJ on 2018-03-06.
+ */
+
+public class Protocol {
+    public static final String SERVICE_TYPE = "_iremember._udp.";
+    public static final String SERVICE_IDENTIFIER = "room";
+}
--- iRememberMaster/app/src/main/java/com/iremember/master/iremembermaster/MainActivity.java
+++ iRememberMaster/app/src/main/java/com/iremember/master/iremembermaster/MainActivity.java
@@ -2,6 +2,10 @@ package com.iremember.master.iremembermaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.iremember.master.iremembermaster.Constants.Commands;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBreakfastClick(View view) {
+        log(this.getApplicationContext().toString());
+        NetworkHandler mNetworkHandler = new NetworkHandler(Commands.BREAKFAST,
+                this.getApplicationContext());
+    }
+
+    public void onLunchClick(View view) {
+    }
+
+    public void onDinnerClick(View view) {
+    }
+
+    public void log(String msg) {
+        Log.d("MainActivity", msg);
+        //Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
     }
 }
