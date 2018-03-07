89f35d6b2bb459cbedddb3965040f236fe1ed161
unknown <kajsaorn@gmail.com>
2018-03-07 17:41:28 +0100

    Add notification to keep foreground thread alive

--- iRememberSubscriber/app/src/main/java/com/iremember/subscriber/iremembersubscriber/NotificationUtils.java
+++ iRememberSubscriber/app/src/main/java/com/iremember/subscriber/iremembersubscriber/NotificationUtils.java
@@ -5,27 +5,30 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 
 public class NotificationUtils {
 
-    public static final String CHANNEL_ID = "com.iremember.subscriber";
-    public static final String CHANNEL_NAME = "iRemember";
+    private final String CHANNEL_ID = "com.iremember.subscriber";
+    private final String CHANNEL_NAME = "iRemember";
+    private int mNotificationId = 0;
 
     public void createNotification(String title, String text, Context context){
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel
+                    (CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW);
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setContentText(title)
                 .setContentText(text)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setPriority(NotificationCompat.PRIORITY_HIGH);
+                .setChannelId(CHANNEL_ID);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            mNotificationManager.createNotificationChannel(mChannel);
-        }
-
-        NotificationManagerCompat mNotificationManagerCompat = NotificationManagerCompat.from(context);
-        mNotificationManagerCompat.notify(0, mBuilder.build());
+        mNotificationManager.notify(mNotificationId++, mBuilder.build());
     }
 }
