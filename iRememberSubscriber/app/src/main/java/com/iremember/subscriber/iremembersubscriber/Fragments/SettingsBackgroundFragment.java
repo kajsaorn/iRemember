4ae420a10a7cc7c0a3f3460c4b294f7502ce4720
unknown <kajsaorn@gmail.com>
2018-03-09 17:43:09 +0100

    gui changes

---
+++ iRememberSubscriber/app/src/main/java/com/iremember/subscriber/iremembersubscriber/Fragments/SettingsBackgroundFragment.java
@@ -0,0 +1,19 @@
+package com.iremember.subscriber.iremembersubscriber.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iremember.subscriber.iremembersubscriber.R;
+
+public class SettingsBackgroundFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings_background, container, false);
+    }
+}
--- iRememberSubscriber/app/src/main/java/com/iremember/subscriber/iremembersubscriber/SettingsActivity.java
+++ iRememberSubscriber/app/src/main/java/com/iremember/subscriber/iremembersubscriber/SettingsActivity.java
@@ -1,33 +1,66 @@
 package com.iremember.subscriber.iremembersubscriber;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.iremember.subscriber.iremembersubscriber.Fragments.SettingsBackgroundFragment;
+import com.iremember.subscriber.iremembersubscriber.Fragments.SettingsInfoFragment;
+import com.iremember.subscriber.iremembersubscriber.Fragments.SettingsMusicFragment;
+import com.iremember.subscriber.iremembersubscriber.Fragments.SettingsTextFragment;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private final int TAB_INFO = R.id.btn_info;
+    private final int TAB_BACKGROUND = R.id.btn_bg_color;
+    private final int TAB_TEXT = R.id.btn_txt_color;
+    private final int TAB_MUSIC = R.id.btn_music;
+    private int mCurrentTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        showFragment(new SettingsInfoFragment());
+        selectTab(TAB_INFO);
     }
 
     public void onInfoClick(View view) {
-        showUserMessage("Info");
+        showFragment(new SettingsInfoFragment());
+        selectTab(TAB_INFO);
     }
 
     public void onBgColorClick(View view) {
-        showUserMessage("Background Color");
+        showFragment(new SettingsBackgroundFragment());
+        selectTab(TAB_BACKGROUND);
     }
 
     public void onTextColorClick(View view) {
-        showUserMessage("Text Color");
+        showFragment(new SettingsTextFragment());
+        selectTab(TAB_TEXT);
     }
 
     public void onMusicClick(View view) {
-        showUserMessage("Music");
+        showFragment(new SettingsMusicFragment());
+        selectTab(TAB_MUSIC);
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+
+    private void selectTab(int tab) {
+        if (mCurrentTab > 0) {
+            findViewById(mCurrentTab).setBackgroundResource(R.drawable.tab_unselected);
+        }
+        findViewById(tab).setBackgroundResource(R.drawable.tab_selected);
+        mCurrentTab = tab;
     }
 
     /**
