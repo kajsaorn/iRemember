2fb3f4b2220e9d9c0523847e82e997bf27b60cb2
unknown <kajsaorn@gmail.com>
2018-04-10 19:15:14 +0200

    Add option to put master IP manually

--- iRememberSubscriber/app/src/main/java/com/iremember/subscriber/iremembersubscriber/Constants/Broadcast.java
+++ iRememberSubscriber/app/src/main/java/com/iremember/subscriber/iremembersubscriber/Constants/Broadcast.java
@@ -6,13 +6,13 @@ public class Broadcast {
     public static final String ROOM_NAME = "room_name";
 
     public static final String MISSING_ROOM_NAME = "no_room_name";
-    public static final String MISSING_SERVICE_NAME = "no_service_name";
+    public static final String MISSING_MASTER_SERVICE = "no_service_name";
 
     public static final String DISCOVERY_FAILURE = "discovery_failure";
     public static final String DISCOVERY_DONE = "discovery_done";
 
     public static final String CONNECTION_SUCCESS = "connection_success";
-    public static final String CONNECTION_FAILURE = "connection_failure";
+    public static final String CONNECTION_SOCKET_FAILURE = "connection_failure";
 
     public static final String DISCONNECTION_FAILURE = "disconnection_failure";
     public static final String DISCONNECTION_SUCCESS = "disconnection_success";
@@ -20,6 +20,7 @@ public class Broadcast {
     public static final String WRONG_WIFI = "wifi_changed";
 
     public static final String SOCKET_FAILURE = "socket_failure";
+    public static final String CONNECTION_HOST_FAILURE = "host_failure";
 
     public static final String SEARCH_MASTER_SERVICE = "search_master";
     public static final String FINISH_REMINDER = "finish_reminder";
